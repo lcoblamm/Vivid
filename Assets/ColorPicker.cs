dd761a1b68be67d40a9cb155358b77aaec473537
lynnel <lynne.lammers@gmail.com>
2015-12-02 00:47:40 -0600

    Adding text display for color picking, fixing camera rendering, and creating unity project with movie file

--- Assets/ColorPicker.cs
+++ Assets/ColorPicker.cs
@@ -1,9 +1,7 @@
 ﻿using UnityEngine;
 using System;
-
-
-
-
+using System.Collections;
+using UnityEngine.UI;
 
 public class ColorPicker : MonoBehaviour {
     ColorPicker colorpicker;
@@ -12,7 +10,10 @@ public class ColorPicker : MonoBehaviour {
     public Texture2D texture = new Texture2D(128, 128);
 
     public Renderer rend;
-    public Boolean selected;
+    public Boolean selected = false;
+	private bool showText = false;
+
+	public Text colorInfo;
 
     //Objects
     public  GameObject Object01  ;
@@ -20,62 +21,59 @@ public class ColorPicker : MonoBehaviour {
 
     public void changeColor()
     {
-
         rend.material.color = Color.Lerp(myColor, Color.white, Time.time);
-
-
     }
 
 
     // Use this for initialization
     public void Start () {
-
-        
         Object01 = GameObject.Find("Plane");
         rend = GetComponent<Renderer>();
-
+		colorInfo.text = "";
     }
 
     // Update is called once per frame
     public void Update () {
-
         //Once color is selected, continue to change frames.
-        if( selected)
+        if (selected)
         {
-
             changeColor();
-
         }
-
-
 
         if (Input.GetMouseButtonDown(0))
         {
-            //Takes the texture2D from the Main Camera.
-            Cam = Camera.main;
-            Cam.Render();
-            Texture2D image = new Texture2D(Cam.targetTexture.width, Cam.targetTexture.height);
-            image.ReadPixels(new Rect(0, 0, Cam.targetTexture.width, Cam.targetTexture.height), 0, 0);
-            image.Apply();
-
-            //Set texture to the captured texture2D.
-            texture = image;
-           
-            //Get the mouse position.
-            int x = Mathf.FloorToInt( Input.mousePosition.x ) ;
-            int y = Mathf.FloorToInt( Input.mousePosition.y );
-            
-            myColor = texture.GetPixel(x, y);
-            selected = true;
-            changeColor();      
+			StartCoroutine(screenGrab());     
         }
 
-
-
-
-
+		if (showText) {
+			colorInfo.text = "The color is " + myColor.ToString();
+		}
     }
 
+	IEnumerator screenGrab() {
+		Debug.Log ("Entered coroutine");
+		yield return new WaitForEndOfFrame();
 
-
+		//Takes the texture2D from the Main Camera.
+		Cam = Camera.main;
+		RenderTexture rTex = new RenderTexture(128, 128, 0);
+		Cam.targetTexture = rTex;
+		Cam.Render();
+		Texture2D image = new Texture2D(Cam.targetTexture.width, Cam.targetTexture.height);
+		image.ReadPixels(new Rect(0, 0, Cam.targetTexture.width, Cam.targetTexture.height), 0, 0);
+		image.Apply();
+		Cam.targetTexture = null;
+		
+		//Set texture to the captured texture2D.
+		texture = image;
+		
+		//Get the mouse position.
+		int x = Mathf.FloorToInt( Input.mousePosition.x ) ;
+		int y = Mathf.FloorToInt( Input.mousePosition.y );
+		
+		myColor = texture.GetPixel(x, y);
+		showText = true;
+		//selected = true;
+		//changeColor(); 
+	}
 }
--- Assets/controller.js
+++ Assets/controller.js
@@ -1,32 +1,20 @@
 ﻿#pragma strict
 
-
 var movTexture : MovieTexture;
 
-
 function Start () {
-
     GetComponent.<Renderer>().material.mainTexture = movTexture; movTexture.Play();
-
 }
 
 function Update () {
 
     if(Input.GetKeyDown(KeyCode.Space)) {
 
-        if(movTexture.isPlaying){
-
-            movTexture.Stop();
-
-        }else{
-
-            movTexture.Play();
-
+        if (movTexture.isPlaying) {
+            movTexture.Pause();
         }
-
-
+        else {
+            movTexture.Play();
+        }
     }
-       
-
-
 }
