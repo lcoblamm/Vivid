73570f1f8f0f771bbcd6314b245602c91e4a06ff
lynnel <lynne.lammers@gmail.com>
2016-02-23 23:34:17 -0600

    Working on adding button - click doesn't do anything currently

--- VividApp/Assets/CameraController.cs
+++ VividApp/Assets/CameraController.cs
@@ -1,20 +1,28 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraController : MonoBehaviour
 {
 	//Objects
-	public GameObject plane;
-	public Camera mainCam;
+	public GameObject plane = null;
 	public WebCamTexture deviceCam = null;
+	[SerializeField] private Button picButton = null;
 
 	// Setting up device camera to render to game plane
 	public void Start () {
 		plane = GameObject.Find("Plane");
-		
 		deviceCam = new WebCamTexture();
 		plane.GetComponent<Renderer>().material.mainTexture = deviceCam;
-		plane.transform.localScale = new Vector3 (1, 1, 1);
 		deviceCam.Play();
+	}
+
+	public void TakeSnapshot() {
+		print ("Entering TakeSnapshot");
+		Texture2D snap = new Texture2D (deviceCam.width, deviceCam.height);
+		snap.SetPixels (deviceCam.GetPixels());
+		snap.Apply ();
+
+		plane.GetComponent<Renderer>().material.mainTexture = snap;
 	}
 }
 
