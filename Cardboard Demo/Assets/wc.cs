90da148d76f10a63f7422b98b9de88b206e17142
jcdziadura <jcdziadura@ku.edu>
2016-02-24 20:47:41 -0600

    Cardboard Demo Added

---
+++ Cardboard Demo/Assets/wc.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wc : MonoBehaviour
+{
+
+    WebCamTexture back;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        back = new WebCamTexture();
+
+        GetComponent<Renderer>().material.mainTexture = back;
+        back.Play();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
