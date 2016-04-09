48b32aafcd8694c1951ff3598fa1d0384b60672b
Alexa Varady <avarady94@gmail.com>
2016-04-09 15:51:50 -0700

    Partially Improved Color Picking
    Needs more data points

--- VividApp/Assets/ColorPicker.cs
+++ VividApp/Assets/ColorPicker.cs
@@ -45,19 +45,33 @@ public class ColorPicker : MonoBehaviour {
         new NamedColor("purple", new Vector4(0.5f, 0, 0.5f, 1)),
         new NamedColor("fuchsia", new Vector4(1, 0, 1, 1)),
         new NamedColor("teal", new Vector4(0, 0.5f, 0.5f, 1)),
-        new NamedColor("aqua", new Vector4(0, 1, 1, 1)),
+        new NamedColor("cyan", new Vector4(0, 1, 1, 1)),
         
         //common "reasonable" colors
         new NamedColor("beige", new Vector4(0.96f, 0.96f, 0.86f, 1)),
         new NamedColor("tan", new Vector4(0.87f, 0.72f, 0.53f, 1)),
         new NamedColor("blue violet", new Vector4(0.54f, 0.17f, 0.89f, 1)),
-        new NamedColor("brown", new Vector4(0.65f, 0.2f, 0.2f, 1)),
+        new NamedColor("brown", new Vector4(0.6f, 0.2f, 0.2f, 1)),
         new NamedColor("pink", new Vector4(1, 0.4f, 0.7f, 1)),
         new NamedColor("yellow green", new Vector4(0.68f, 1, 0.18f, 1)),
         new NamedColor("lavender", new Vector4(0.9f, 0.9f, 0.98f, 1)),
         new NamedColor("light blue", new Vector4(0.68f, 0.85f, 0.9f, 1)),
         new NamedColor("light green", new Vector4(0.56f, 0.93f, 0.56f, 1)),
-        new NamedColor("orange", new Vector4(1, 0.65f, 0, 1))
+        new NamedColor("orange", new Vector4(1, 0.65f, 0, 1)),
+        new NamedColor("aquamarine", new Vector4(0.5f, 1, 0.83f, 1)),
+        new NamedColor("cornflower blue", new Vector4(0.39f, 0.58f, 0.93f, 1)),
+        new NamedColor("coral", new Vector4(1, 0.5f, 0.31f, 1)),
+        new NamedColor("salmon", new Vector4(0.98f, 0.5f, 0.45f, 1)),
+        new NamedColor("crimson", new Vector4(0.86f, 0.08f, 0.24f, 1)),
+        new NamedColor("hot pink", new Vector4(1, 0.08f, 0.58f, 1)),
+        new NamedColor("forest green", new Vector4(0.13f, 0.55f, 0.13f, 1)),
+        new NamedColor("ivory", new Vector4(1, 1, 0.94f, 1)),
+        new NamedColor("golden yellow", new Vector4(1, 0.84f, 0, 1)),
+        new NamedColor("yellow green", new Vector4(0.6f, 0.9f, 0.18f, 1)),
+        new NamedColor("indigo", new Vector4(0.29f, 0, 0.51f, 1)),
+        new NamedColor("red orange", new Vector4(1, 0.3f, 0, 1)),
+        new NamedColor("purple", new Vector4(0.63f, 0.13f, 0.94f, 1)),
+        new NamedColor("sky blue", new Vector4(0.53f, 0.81f, 0.92f, 1))
     };
 
     // Use this for initialization
@@ -177,13 +191,24 @@ public class ColorPicker : MonoBehaviour {
 		myColor = image.GetPixel(x,y); 
 
 		Debug.Log ("Pixel RGB: (" + myColor.r * 256 + ", " + myColor.g * 256 + ", " + myColor.b * 256 + ")");
-		
-		colorInfo.text = "The closest color is " + getColorName(myColor.gamma);
+        Color c1 = image.GetPixel(x - 1, y);
+        Color c2 = image.GetPixel(x + 1, y);
+        Color c3 = image.GetPixel(x - 1, y - 1);
+        Color c4 = image.GetPixel(x + 1, y - 1);
+        Color c5 = image.GetPixel(x - 1, y + 1);
+        Color c6 = image.GetPixel(x + 1, y + 1);
+        Color c7 = image.GetPixel(x, y - 1);
+        Color c8 = image.GetPixel(x, y + 1);
+
+        double red = (c1.r + c2.r + c3.r + c4.r + c5.r + c6.r + c7.r + c8.r + myColor.r) / 9;
+        double green = (c1.g + c2.g + c3.g + c4.g + c5.g + c6.g + c7.g + c8.g + myColor.g) / 9;
+        double blue = (c1.b + c2.b + c3.b + c4.b + c5.b + c6.b + c7.b + c8.b + myColor.b) / 9;
+        colorInfo.text = "The closest color is " + getColorName(red, green, blue);
     }
 
-    private string getColorName(Color color) {
+    private string getColorName(double r, double g, double b) {
         //Check for greyscale first
-        if(Math.Abs(color.r-color.g) + Math.Abs(color.r-color.b) + Math.Abs(color.g-color.b) < 0.1) {
+        if(Math.Abs(r-g) + Math.Abs(r-b) + Math.Abs(g-b) < 0.1) {
             return "grey";
         }
         //Find closest color
@@ -193,9 +218,9 @@ public class ColorPicker : MonoBehaviour {
         int smallestIndex = -1;
         for(int i=0; i<colorList.Length; i++) {
             dist =
-                (color.r - colorList[i].rgba[0]) * (color.r - colorList[i].rgba[0])
-                + (color.g - colorList[i].rgba[1]) * (color.g - colorList[i].rgba[1])
-                + (color.b - colorList[i].rgba[2]) * (color.b - colorList[i].rgba[2]);
+                (r - colorList[i].rgba[0]) * (r - colorList[i].rgba[0])
+                + (g - colorList[i].rgba[1]) * (g - colorList[i].rgba[1])
+                + (b - colorList[i].rgba[2]) * (b - colorList[i].rgba[2]);
             if(dist < smallestDist) {
                 smallestDist = dist;
                 smallestIndex = i;
