5c1da5340751fba1ce532e37a64e57a78e3121c8
Josh Albrecht <joshmalbrecht@gmail.com>
2016-04-11 14:16:42 -0500

    Added comments to start menu.

--- VividApp/Assets/startMenu.cs
+++ VividApp/Assets/startMenu.cs
@@ -11,20 +11,17 @@ public class startMenu : MonoBehaviour {
 	public int state ; 
 
 	public GameObject menu ;
-
-	//GameObject plane = GameObject.Find("Plane");
-	//ColorPicker variable = plane.GetComponent<ColorPicker>();
-
-	//ColorPicker variable = new ColorPicker() ; 
-	//public Renderer rend;
+	
 
 	// Use this for initialization
 	void Start () {
 	
+		//Needed so that state variable will not be lost in the transistion from the start menu into the main scene. 
 		DontDestroyOnLoad(transform.gameObject);
 
 	}
 
+	//Red Green button Selected, set set state to 1, and load main scene.
 	public void RedGreenPress() {
 
 		state = 1 ; 
@@ -32,6 +29,8 @@ public class startMenu : MonoBehaviour {
 		Application.LoadLevel (1); 
 		menu.SetActive (false); 
 	}
+
+	//Blue Yellow button Selected, set set state to 2, and load main scene.
 	public void BlueYellowPress() {
 
 		state = 2 ; 
@@ -43,6 +42,7 @@ public class startMenu : MonoBehaviour {
 
 	}
 
+	//No shadder button Selected, set set state to 0, and load main scene.
 	public void NonePress() {
 
 		state = 0 ; 
