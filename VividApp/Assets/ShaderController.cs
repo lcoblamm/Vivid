a62a0f0401d41752c0842d93d1efa4c35a3b8882
lcoblamm <lynne.coblammers@gmail.com>
2016-04-12 15:53:32 -0500

    Making slider disappear when mode None is selected and adding prompt on menu

--- VividApp/Assets/ShaderController.cs
+++ VividApp/Assets/ShaderController.cs
@@ -7,21 +7,16 @@ public class ShaderController : MonoBehaviour {
 
 	public float sliderPosition;
 	public Renderer rend;
+	public GameObject slider;
 
 	// Use this for initialization
 	void Start () {
-
-
 		rend = GetComponent<Renderer>();
-
+		slider = GameObject.Find ("Slider");
 		//If the "None" option was selected in the start menu.
 		if (GlobalControl.Instance.state == 0) {
-
-
-
-
+			slider.SetActive(false);
 		}
-	
 	}
 
 	public void OnValueChanged(float pos) {
@@ -30,8 +25,6 @@ public class ShaderController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-
-			rend.material.SetFloat ("_SliderValue", sliderPosition);
-
+		rend.material.SetFloat ("_SliderValue", sliderPosition);
 	}
 }
