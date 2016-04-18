02c552edcd7cfadb706667fc914ff360f64d1b3c
lcoblamm <lynne.coblammers@gmail.com>
2016-04-18 18:11:09 -0500

    Missed a few files on the last commit

---
+++ VividApp/Assets/ModeEnum.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Mode {
+	ColorPicker = 0,
+	RedGreen = 1, 
+	BlueYellow = 2,
+	HighContrast = 3,
+	Negative = 4,
+	RedGreenSim = 11,
+	BlueYellowSim = 12,
+	HueShift = 20
+};
+
+public class ModeEnum : MonoBehaviour {
+	public enum Mode {
+		ColorPicker = 0,
+		RedGreen = 1, 
+		BlueYellow = 2,
+		HighContrast = 3,
+		Negative = 4,
+		RedGreenSim = 11,
+		BlueYellowSim = 12,
+		HueShift = 20
+	};
+}
