56e39fec787c02ea6333f66370463e7fa07394f5
lcoblamm <lynne.coblammers@gmail.com>
2016-04-18 20:41:53 -0500

    Creating ad hoc splash screen - make sure to add the new scene to your build

---
+++ VividApp/Assets/SplashScreenController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreenController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ();
+		Application.LoadLevel ("Menu");
+	}
+
+	IEnumerator StartCoroutine() {
+		yield return new WaitForSeconds(3);
+	}
+}
